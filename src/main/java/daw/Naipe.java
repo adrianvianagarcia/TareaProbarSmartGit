a4c58c853a755499713267fe61dda0f3aa3cf23d
adrianviana <aviagar307@g.educaand.es>
2023-12-01 10:03:04 +0100

    Cosntructor predeterminado

--- src/main/java/daw/Naipe.java
+++ src/main/java/daw/Naipe.java
@@ -1,5 +1,20 @@
 package daw;
 
+import java.util.Random;
+
 public class Naipe {
 
+    String numeroCarta;
+    String palo;
+
+    public Naipe() {
+
+        Random r = new Random();
+        String[] numeroCarta = {"1", "2", "3", "4", "5", "6", "7", "sota", "caballo",
+            "rey"};
+        String[] palo = {"oros", "bastos", "espadas", "copas"};
+
+        this.numeroCarta = numeroCarta[r.nextInt(0, 11)];
+        this.palo = palo[r.nextInt(0, 5)];
+    }
 }
